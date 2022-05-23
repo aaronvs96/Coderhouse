cb500f507ba64987351b53e83b7df20e2d6c8102
Aarón Vásquez Sánchez <30508004+aaronvs96@users.noreply.github.com>
2022-05-23 16:12:01 -0500

    Update catalogo.js

--- proyecto_final_JS/js/catalogo.js
+++ proyecto_final_JS/js/catalogo.js
@@ -144,10 +144,4 @@ selectFiltro.addEventListener("change", () => {
 
 
 //FORMATO DOLARES
-const formatoDolares = (monto) => {
-    const options2 = { style: 'currency', currency: 'USD' };
-    const numberFormat2 = new Intl.NumberFormat('en-US', options2);
-
-    // console.log(numberFormat2.format(monto));
-    return numberFormat2.format(monto);
-}
+import {formatoDolares} from './formatoDolares.js';
